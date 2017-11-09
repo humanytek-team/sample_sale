9b6a5ad2f7c671a68b25a57112e4d34b2f670fbc
Marco Cid <marcocb46@gmail.com>
2017-11-08 21:01:44 -0600

    cambios

--- invoice.py
+++ invoice.py
@@ -8,60 +8,60 @@ class AccountInvoice(models.Model):
     _inherit = "account.invoice"
 
 
-    @api.multi
-    def get_taxes_values(self):
-        print 'get_taxes_values'
-        if self.sample:
-            tax_grouped = {}
-            for line in self.invoice_line_ids:
-                #price_unit = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-                price_unit = _COST * (1 - (line.discount or 0.0) / 100.0)
+    # @api.multi
+    # def get_taxes_values(self):
+    #     print 'get_taxes_values'
+    #     if self.sample:
+    #         tax_grouped = {}
+    #         for line in self.invoice_line_ids:
+    #             #price_unit = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+    #             price_unit = _COST * (1 - (line.discount or 0.0) / 100.0)
 
-                taxes = line.invoice_line_tax_ids.compute_all(price_unit, self.currency_id, line.quantity, line.product_id, self.partner_id)['taxes']
-                for tax in taxes:
-                    val = self._prepare_tax_line_vals(line, tax)
-                    key = self.env['account.tax'].browse(tax['id']).get_grouping_key(val)
+    #             taxes = line.invoice_line_tax_ids.compute_all(price_unit, self.currency_id, line.quantity, line.product_id, self.partner_id)['taxes']
+    #             for tax in taxes:
+    #                 val = self._prepare_tax_line_vals(line, tax)
+    #                 key = self.env['account.tax'].browse(tax['id']).get_grouping_key(val)
 
-                    if key not in tax_grouped:
-                        tax_grouped[key] = val
-                    else:
-                        tax_grouped[key]['amount'] += val['amount']
-                        tax_grouped[key]['base'] += val['base']
-            return tax_grouped
-        else:
-            super(AccountInvoice,self).get_taxes_values()
+    #                 if key not in tax_grouped:
+    #                     tax_grouped[key] = val
+    #                 else:
+    #                     tax_grouped[key]['amount'] += val['amount']
+    #                     tax_grouped[key]['base'] += val['base']
+    #         return tax_grouped
+    #     else:
+    #         super(AccountInvoice,self).get_taxes_values()
 
-    @api.one
-    @api.depends('invoice_line_ids.price_subtotal', 'tax_line_ids.amount', 'currency_id', 'company_id', 'date_invoice', 'type', 'sample')
-    def _compute_amount(self):
-        #print '_compute_amount'
-        if self.sample:
-            #self.amount_untaxed = sum(line.price_subtotal for line in self.invoice_line_ids)
-            #self.amount_untaxed = 0.01
-            self.amount_untaxed = sum((line.quantity * _COST) for line in self.invoice_line_ids)
+    # @api.one
+    # @api.depends('invoice_line_ids.price_subtotal', 'tax_line_ids.amount', 'currency_id', 'company_id', 'date_invoice', 'type', 'sample')
+    # def _compute_amount(self):
+    #     #print '_compute_amount'
+    #     if self.sample:
+    #         #self.amount_untaxed = sum(line.price_subtotal for line in self.invoice_line_ids)
+    #         #self.amount_untaxed = 0.01
+    #         self.amount_untaxed = sum((line.quantity * _COST) for line in self.invoice_line_ids)
 
-            #self.amount_tax = sum(line.amount for line in self.tax_line_ids)
-            #self.amount_tax = 0.0
-            self.amount_tax = sum(line.amount for line in self.tax_line_ids)
+    #         #self.amount_tax = sum(line.amount for line in self.tax_line_ids)
+    #         #self.amount_tax = 0.0
+    #         self.amount_tax = sum(line.amount for line in self.tax_line_ids)
 
-            #self.amount_total = self.amount_untaxed + self.amount_tax
-            #self.amount_total = 0.01
-            self.amount_total = self.amount_untaxed + self.amount_tax
+    #         #self.amount_total = self.amount_untaxed + self.amount_tax
+    #         #self.amount_total = 0.01
+    #         self.amount_total = self.amount_untaxed + self.amount_tax
 
-            amount_total_company_signed = self.amount_total
-            amount_untaxed_signed = self.amount_untaxed
+    #         amount_total_company_signed = self.amount_total
+    #         amount_untaxed_signed = self.amount_untaxed
 
-            if self.currency_id and self.company_id and self.currency_id != self.company_id.currency_id:
-                currency_id = self.currency_id.with_context(date=self.date_invoice)
-                amount_total_company_signed = currency_id.compute(self.amount_total, self.company_id.currency_id)
-                amount_untaxed_signed = currency_id.compute(self.amount_untaxed, self.company_id.currency_id)
-            sign = self.type in ['in_refund', 'out_refund'] and -1 or 1
-            self.amount_total_company_signed = amount_total_company_signed * sign
-            self.amount_total_signed = self.amount_total * sign
-            self.amount_untaxed_signed = amount_untaxed_signed * sign
-        else:
-            super(AccountInvoice,self)._compute_amount()
-        #print 'end'
+    #         if self.currency_id and self.company_id and self.currency_id != self.company_id.currency_id:
+    #             currency_id = self.currency_id.with_context(date=self.date_invoice)
+    #             amount_total_company_signed = currency_id.compute(self.amount_total, self.company_id.currency_id)
+    #             amount_untaxed_signed = currency_id.compute(self.amount_untaxed, self.company_id.currency_id)
+    #         sign = self.type in ['in_refund', 'out_refund'] and -1 or 1
+    #         self.amount_total_company_signed = amount_total_company_signed * sign
+    #         self.amount_total_signed = self.amount_total * sign
+    #         self.amount_untaxed_signed = amount_untaxed_signed * sign
+    #     else:
+    #         super(AccountInvoice,self)._compute_amount()
+    #     #print 'end'
 
 
     sample = fields.Boolean('Sample')
--- sale.py
+++ sale.py
@@ -9,6 +9,18 @@ class SaleOrder(models.Model):
     _inherit = "sale.order"
 
 
+    @api.multi
+    @api.onchange('sample')
+    def onchange_sample(self):
+        for order in self:
+            if order.order_line:
+                if order.sample:
+                    for line in order.order_line:
+                        line.price_unit = _COST
+                else:
+                    for line in order.order_line:
+                        line.price_unit = line.product_id.lst_price
+
     sample = fields.Boolean('Sample')
 
 
@@ -38,56 +50,56 @@ class SaleOrder(models.Model):
     #         super(SaleOrder,self)._amount_all()
     #     #print 'end'
 
-    @api.depends('order_line.price_total','sample')
-    def _amount_all(self):
-        print '_amount_all'
+    # @api.depends('order_line.price_total','sample')
+    # def _amount_all(self):
+    #     print '_amount_all'
         
-        #CHECKS IF SALE ORDER IS SAMPLE
-        if self.sample:
-            for order in self:
-                amount_untaxed = amount_tax = 0.0
-                for line in order.order_line:
+    #     #CHECKS IF SALE ORDER IS SAMPLE
+    #     if self.sample:
+    #         for order in self:
+    #             amount_untaxed = amount_tax = 0.0
+    #             for line in order.order_line:
 
-                    #amount_untaxed += line.price_subtotal
-                    amount_untaxed += (line.product_uom_qty * _COST)
+    #                 #amount_untaxed += line.price_subtotal
+    #                 amount_untaxed += (line.product_uom_qty * _COST)
 
-                    # FORWARDPORT UP TO 10.0
-                    if order.company_id.tax_calculation_rounding_method == 'round_globally':
+    #                 # FORWARDPORT UP TO 10.0
+    #                 if order.company_id.tax_calculation_rounding_method == 'round_globally':
 
-                        #price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-                        price = _COST * (1 - (line.discount or 0.0) / 100.0)
+    #                     #price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+    #                     price = _COST * (1 - (line.discount or 0.0) / 100.0)
 
-                        taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=order.partner_shipping_id)
-                        amount_tax += sum(t.get('amount', 0.0) for t in taxes.get('taxes', []))
-                    else:
-                        amount_tax += line.price_tax
-                order.update({
-                    'amount_untaxed': order.pricelist_id.currency_id.round(amount_untaxed),
-                    'amount_tax': order.pricelist_id.currency_id.round(amount_tax),
-                    'amount_total': amount_untaxed + amount_tax,
-                })
-        else:
-            super(SaleOrder,self)._amount_all()
-
-
-class SaleOrderLine(models.Model):
-    _inherit = 'sale.order.line'
+    #                     taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=order.partner_shipping_id)
+    #                     amount_tax += sum(t.get('amount', 0.0) for t in taxes.get('taxes', []))
+    #                 else:
+    #                     amount_tax += line.price_tax
+    #             order.update({
+    #                 'amount_untaxed': order.pricelist_id.currency_id.round(amount_untaxed),
+    #                 'amount_tax': order.pricelist_id.currency_id.round(amount_tax),
+    #                 'amount_total': amount_untaxed + amount_tax,
+    #             })
+    #     else:
+    #         super(SaleOrder,self)._amount_all()
 
 
-    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id', 'order_id.sample')
-    def _compute_amount(self):
-        print '_compute_amount'
-        super(SaleOrderLine,self)._compute_amount()
-        for line in self:
-            if line.order_id.sample:
-                #price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-                price = _COST * (1 - (line.discount or 0.0) / 100.0)
-                taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
-                line.update({
-                    'price_tax': taxes['total_included'] - taxes['total_excluded'],
-                    'price_total': taxes['total_included'],
-                    'price_subtotal': taxes['total_excluded'],
-                })
+# class SaleOrderLine(models.Model):
+#     _inherit = 'sale.order.line'
+
+
+#     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id', 'order_id.sample')
+#     def _compute_amount(self):
+#         print '_compute_amount'
+#         super(SaleOrderLine,self)._compute_amount()
+#         for line in self:
+#             if line.order_id.sample:
+#                 #price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+#                 price = _COST * (1 - (line.discount or 0.0) / 100.0)
+#                 taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
+#                 line.update({
+#                     'price_tax': taxes['total_included'] - taxes['total_excluded'],
+#                     'price_total': taxes['total_included'],
+#                     'price_subtotal': taxes['total_excluded'],
+#                 })
 
     # @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
     # def _compute_amount(self):
